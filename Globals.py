1be48fbf6cf2e95e21342550080167206ef5e8b5
ethan0807 <43458694+ethan0807@users.noreply.github.com>
2023-10-09 21:02:53 -0400

    Update Globals.py
    Switch to GPT-4

--- Globals.py
+++ Globals.py
@@ -6,12 +6,12 @@ texts_version = "v1"
 
 class Defaults:
     
-    default_model = "gpt-3.5-turbo"  # chunk limit 4096
-    # "gpt-4"  chunk limit 8192
+    default_model = "gpt-4" # chunk limit 8192 
+    # "gpt-3.5-turbo"  chunk limit 4096
     # "text-davinci-003" chunk limit 4097
     # "ada"
     default_max_chunk_size = 2048
-    default_temperature = 0
+    default_temperature = 0.1
 
     index_path = "./data/index/" + index_version + "/"
     texts_path = "./data/parsed_sections/" + texts_version + "/"
